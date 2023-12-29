e6b64ce7793b159ddfb1b0c74e02b50441bba54e
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2023-12-30 02:54:29 +0530

    tiles are now dynamically added using Listview.builder

--- lib/homa_page.dart
+++ lib/homa_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utilities/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // List of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do Exercise", false],
+    ["Play Cricket",true],
+    ["Do DSA",true]
+  ];
+ 
+   // checkBox  tapped
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[200],
       appBar: AppBar(
-         backgroundColor: Colors.yellow,
+        backgroundColor: Colors.yellow,
+        title: Center(child: Text('TO DO')),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TODOTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged : (value) => checkBoxChanged,
+              );
+        },
       ),
     );
   }
